9037f79152e29e1b0d213ea89c554d9366e2cc67
K1493 <K1493@student.jamk.fi>
2017-02-09 10:35:02 +0200

    AS01 tehty

--- Demo05/AS01/Program.cs
+++ Demo05/AS01/Program.cs
@@ -10,7 +10,59 @@ namespace AS01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("kakat");
+            // create Persons (colletcion)
+            Persons myFriends = new Persons();
+
+            //create a few Personobjects
+            Person person1 = new Person
+            {
+                Firstname = "Kirsi",
+                Lastname = "Kernel",
+                SocialSecurityNumber = "12-12-12-1-2"
+            };
+
+            Person person2 = new Person
+            {
+                Firstname = "Pekka",
+                Lastname = "Pouta",
+                SocialSecurityNumber = "k1493-dddddddd"
+            };
+
+            Person person3 = new Person
+            {
+                Firstname = "Jaska",
+                Lastname = "Jokunen",
+                SocialSecurityNumber = "666"
+            };
+            //add Person objects to Persons(myFriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            //get one person
+            Person person4 = myFriends.GetPerson(0);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("person not found in positon");
+            }
+            myFriends.PrintData();
+            //FindPerson
+            string SocialSecurityNumber = "666";
+            Console.WriteLine("Find  Person with ssn: " + SocialSecurityNumber);
+            Person person5 = myFriends.FindPerson(SocialSecurityNumber);
+
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("cant find person with that ssn");
+            }
         }
     }
 }
